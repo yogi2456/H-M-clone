cb52478e9ba350ee676765610977110cb095dd91
yogi2456 <138296327+yogi2456@users.noreply.github.com>
2023-10-03 12:09:10 +0530

    updates

--- frontend/src/Components/Mens.js
+++ frontend/src/Components/Mens.js
@@ -4,48 +4,7 @@ import './Mens.css'
 const Mens = () => {
     return (
         <>
-        <div>
-        <div className="navbar">
-            <div>
-                <div className="custom">
-                    <p>Customer Service</p>
-                    <p>Newsletter</p>
-                    <p>Find a store</p>
-                </div>
-                <div className="hm">
-                    <p>H & M</p>
-                </div>
-                <div className="sign">
-                    <p><i class="fa-regular fa-user fa-xl" style={{color: "#121212"}}></i></p>
-                    <p>Sign in</p>
-                    <p><i class="fa-regular fa-heart fa-xl" style={{color: "#00080f"}}></i></p>
-                    <p>Favourite</p>
-                    <p><i class="fa-solid fa-bag-shopping fa-xl" style={{color: "#1a1a1a"}}></i></p>
-                    <p>Shopping bag (0)</p>
-                </div>
-            </div>
-        </div>
-
-        <div className="leftright">
-            <div className="left">
-                <p>Ladies</p>
-                <p>Men</p>
-                <p>Divided</p>
-                <p>Baby</p>
-                <p>Kids</p>
-                <p>H&M HOME</p>
-                <p>Sport</p>
-                <p>Sustainability</p>
-                <p>Sale</p>
-            </div>
-            <div className="right">
-                <div>
-                    <p><i class="fa-solid fa-magnifying-glass fa-xl" style={{color: "#090404"}}></i></p>
-                    <p>Search Products</p>
-                </div>
-                <div></div>
-            </div>
-        </div>
+        <div className="screen">
 
         <div className="member10">
             <div>
@@ -133,58 +92,6 @@ const Mens = () => {
                     <h2>Men's Clothing</h2>
                     <p>Check out all the freshest styles your closet needs in our men's clothing range. You'll find a roundup of everyday essentials, including tops and T-Shirts, as well as comfy lounge sets and underwear. Formal event coming up? Scroll no further than our men's blazers and suits for the sharpest looks and nail the dress code. When it comes to men's pants, there's chinos, joggers and cargo styles in all the staple colors. dreaming of denim? our men's jeans offer a range of fits to suit your style, including skinny,</p>
                     <p>Read more</p>
-                </div>
-            </div>
-        </div>
-
-        <div className="shop">
-            <div className="shop1">
-                <div className="section">
-                    <h3>Shop</h3>
-                    <p>Ladies</p>
-                    <p>Men</p>
-                    <p>Baby</p>
-                    <p>Kids</p>
-                    <p>H&M HOME</p>
-                    <p>Sport</p>
-                </div>
-                <div className="section1">
-                    <h3>CORPORATE INFO</h3>
-                    <p>Career at H&M</p>
-                    <p>About H&M group</p>
-                    <p>Sustainability H&M Group</p>
-                    <p>Press</p>
-                    <p>Investor relations</p>
-                    <p>Corporate governance</p>
-                </div>
-                <div className="section2">
-                    <h3>HELP</h3>
-                    <p>Customer Service</p>
-                    <p>My H&M</p>
-                    <p>Find a store</p>
-                    <p>Legal & Privacy</p>
-                    <p>Contact</p>
-                    <p>Report a scam</p>
-                    <p>Cookie Notice</p>
-                    <p>Cookie Settings</p>
-                </div>
-                <div className="section3">
-                    <p>Sign up now and be the first to know  <br/> about exclusive offers, latest fashion <br/> news & style tips!</p>
-                    <p>Read more</p>
-                </div>
-            </div>
-            <div className="icons">
-                <div className="icons1">
-                    <p><i class="fa-brands fa-facebook"></i></p>
-                    <p><i class="fa-brands fa-twitter"></i></p>
-                    <p><i class="fa-brands fa-instagram"></i></p>
-                    <p><i class="fa-brands fa-youtube"></i></p>
-                    <p><i class="fa-brands fa-pinterest"></i></p>
-                </div>
-                <div className="icons2">
-                    <p>The content of this site is copyright-protected and is the property of H & M Hennes & Mauritz AB.H&M</p>
-                    <p>H&M</p>
-                    <p>INDIA | Rs.</p>
                 </div>
             </div>
         </div>
